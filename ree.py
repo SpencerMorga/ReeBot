a6a15a6b7d718bcb153befa964baab247ed4b1b1
Ryan Scopio <ryanscopio@gmail.com>
2017-11-17 23:07:49 -0800

    Made the token a command line input. Also should respond to all rooms. Also added hidden messages

--- ree.py
+++ ree.py
@@ -1,13 +1,19 @@
 import discord
 import random
+import sys
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Mother give me tendies')
 
     async def on_message(self, message):
-        if(message.mention_everyone):
-            await client.send_file(client.get_channel('301575084630343681'), random.choice(['a.jpg','b.jpg','c.jpg','d.jpg','e.jpg','f.jpg','g.gif']))
+        if message.mention_everyone:
+            await client.send_file(message.channel, random.choice(['a.jpg','b.jpg','c.jpg','d.jpg','e.jpg','f.jpg','g.gif']))
+        if message.content.startswith('@NerdWords#9998'):
+            await client.send_message(message.channel, 'papa 🖐👁👄👁🖐')
+        if message.content.startswith('@wSpitfire#0344'):
+            await client.send_message(message.channel, 'mama 🖐👁👄👁🖐')
 
+token = sys.argv[1]
 client = MyClient()
-client.run('Mzc4NDI3MTQ3MzkzNTY0Njgz.DObZeA.A03xbIdbAKHMovgMxYsYoc6EILk')
+client.run(token)
